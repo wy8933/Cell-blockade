05241d1e1bcf87c15ec87e5619478af37b12ccc2
wy8933 <112401719+wy8933@users.noreply.github.com>
2025-04-20 22:36:01 -0400

    Done with shop localization

---
+++ GGJ2025/GGJ2025/Assets/Scripts/Util/LocalizationManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+using System.Collections;
+
+public class LocalizationManager : MonoBehaviour
+{
+    public static LocalizationManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public string GetLocalizedString(string tableName, string entryKey)
+    {
+        var table = LocalizationSettings.StringDatabase.GetTable(tableName);
+        if (table == null)
+        {
+            Debug.LogError($"Localization Table '{tableName}' not found!");
+            return $"[{entryKey}]";
+        }
+
+        var entry = table.GetEntry(entryKey);
+        if (entry == null)
+        {
+            Debug.LogError($"Entry '{entryKey}' not found in Table '{tableName}'!");
+            return $"[{entryKey}]";
+        }
+
+        return entry.GetLocalizedString();
+    }
+}
